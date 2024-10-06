5ce0afdb53d3d9fa03a9b1d4a81e93aa50c563d7
devinachilly <devinachilly22@gmail.com>
2024-10-06 12:32:04 +0700

    Update dpa_function.cpp

--- dpa_function.cpp
+++ dpa_function.cpp
@@ -17,6 +17,17 @@ double luasSegitiga(double alas, double tinggi) {
 float luasJajarGenjang(float alas, float tinggi) {
     return alas * tinggi ;
 }
+
+double luasTrapesium(double a, double b, double tinggi) {
+    return 0.5 * (a + b) * tinggi;
+}
+
+double luasBelahKetupat(double diagonal1, double diagonal2) {
+    return 0.5 * diagonal1 * diagonal2;
+}
+
+}
+    
 int main(){
 int jari;
 cout << "masukkan jari-jari" << endl;
@@ -24,7 +35,7 @@ cin >> jari;
 
 cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
 
-  double alas, tinggi, luas;
+    double alas, tinggi, luas;
 
     cout << "Masukkan panjang alas segitiga: ";
     cin >> alas;
@@ -41,7 +52,24 @@ cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
     
     luas = luasJajarGenjang(alas, tinggi) ;
     cout << "Luas jajargenjang : " << luas << endl;
-    
+
+    double a, b, tinggii, diagonal1, diagonal2;
+
+    cout << "Masukkan panjang sisi sejajar dari trapesium (a dan b): ";
+    cin >> a >> b;
+    cout << "Masukkan tinggi dari trapesium: ";
+    cin >> tinggi;
+
+    double luas_trapesium = luasTrapesium(a, b, tinggi);
+    cout << "Luas trapesium adalah: " << luas_trapesium << endl;
+
+    cout << "\nMasukkan panjang diagonal 1 dari belah ketupat: ";
+    cin >> diagonal1;
+    cout << "Masukkan panjang diagonal 2 dari belah ketupat: ";
+    cin >> diagonal2;
+
+    double luas_belah_ketupat = luasBelahKetupat(diagonal1, diagonal2);
+    cout << "Luas belah ketupat adalah: " << luas_belah_ketupat << endl;
 
 return 0;
 }
