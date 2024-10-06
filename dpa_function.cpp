a7012b061d11aecc1c3acb136c06910bcc4a605d
dhemasravinagata <dravinagata@gmail.com>
2024-10-06 12:07:37 +0700

    Update dpa_function.cpp

--- dpa_function.cpp
+++ dpa_function.cpp
@@ -9,6 +9,14 @@ return pow(jari,2)*3.14;
 double luaspersegi(double sisi){
 return pow(sisi,2);
 }
+
+double luasSegitiga(double alas, double tinggi) {
+    return 0.5 * alas * tinggi;
+}
+
+float luasJajarGenjang(float alas, float tinggi) {
+    return alas * tinggi ;
+}
 int main(){
 int jari;
 cout << "masukkan jari-jari" << endl;
@@ -16,6 +24,24 @@ cin >> jari;
 
 cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
 
+  double alas, tinggi, luas;
+
+    cout << "Masukkan panjang alas segitiga: ";
+    cin >> alas;
+    cout << "Masukkan tinggi segitiga: ";
+    cin >> tinggi;
+
+    luas = luasSegitiga(alas, tinggi);
+    cout << "Luas segitiga adalah: " << luas << endl;
+     
+    cout << "Masukkan panjang alas jajargenjang : ";
+    cin >> alas;
+    cout << "Masukkan tinggi jajar genjang : " ;
+    cin >> tinggi;
+    
+    luas = luasJajarGenjang(alas, tinggi) ;
+    cout << "Luas jajargenjang : " << luas << endl;
+    
 
 return 0;
 }
