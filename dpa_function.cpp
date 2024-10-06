f40bc912279edf40ce882c79bffa997eba92f235
davaismifadil <davaismif@gmail.com>
2024-10-06 14:27:39 +0700

    menambahkan if dan looping

--- dpa_function.cpp
+++ dpa_function.cpp
@@ -26,29 +26,60 @@ double luasBelahKetupat(double diagonal1, double diagonal2) {
 }
     
 int main(){
-int jari;
+    char keputusan;
+    do{
+    cout << "1.Luas lingkaran" << endl;
+    cout << "2.Luas persegi" << endl;
+    cout << "3.Luas segitiga" << endl;
+    cout << "4.Luas jajar genjang" << endl;
+    cout << "5.Luas trapesium" << endl;
+    cout << "6.Luas belah ketupat" << endl;
+    cout << "\n";
+    int pilihan;
+    cout << "masukkan angka pilihan"<< endl;
+    cin >> pilihan;
+
+if(pilihan == 1){   
+double jari;
 cout << "masukkan jari-jari lingkaran : " << endl;
 cin >> jari;
 
 cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
-
+    cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+}
+if (pilihan == 2){
+    double sisi;
+        cout << "masukkan sisi" <<endl;
+        cin >> sisi;
+        cout << "Hasilnya adalah : "<< luaspersegi(sisi) << endl;
+        cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+}
+    if(pilihan == 3){
     double alas, tinggi, luas;
     cout << "Masukkan panjang alas segitiga : " ;
     cin >> alas;
     cout << "Masukkan tinggi segitiga : " ;
         cin >> tinggi;
     luas = luasSegitiga (alas, tinggi);
-        cout << "Luas segitiga adalah : " << luas <<endl ;
-
+        cout << "Luas segitiga adalah : " << luas <<endl;
+        cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+    }
+    if(pilihan == 4){
+        double alas,tinggi,luas;
         cout << "Masukkan panjang alas jajar genjang : " ;
         cin >> alas;
         cout << "Masukkan tinggi jajar genjang : " ;
         cin >> tinggi;
         luas = luasJajargenjang (alas, tinggi) ;
         cout << " Luas jajar genjang adalah " << luas << endl;
-        
-
-    double a, b, tinggii, diagonal1, diagonal2;
+   cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+    }
+if(pilihan == 5){
+    double a, b, tinggi;
 
     cout << "Masukkan panjang sisi sejajar dari trapesium (a dan b): ";
     cin >> a >> b;
@@ -57,7 +88,11 @@ cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
 
     double luas_trapesium = luasTrapesium(a, b, tinggi);
     cout << "Luas trapesium adalah: " << luas_trapesium << endl;
-
+cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+}
+    if(pilihan == 6){
+        double diagonal1,diagonal2;
     cout << "\nMasukkan panjang diagonal 1 dari belah ketupat: ";
     cin >> diagonal1;
     cout << "Masukkan panjang diagonal 2 dari belah ketupat: ";
@@ -65,6 +100,12 @@ cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
 
     double luas_belah_ketupat = luasBelahKetupat(diagonal1, diagonal2);
     cout << "Luas belah ketupat adalah: " << luas_belah_ketupat << endl;
-    
+   cout << "berhitung lagi?(y/n)" << endl;
+    cin >> keputusan;
+    }
+        else{
+            cout << "pilihan tidak sesuai" << endl;
+        }
+}while(keputusan == 'y');
 return 0;
 }
