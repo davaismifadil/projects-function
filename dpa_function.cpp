283c24d2dc8171421f32b3fdda3714b9311e3c05
davaismifadil <davaismif@gmail.com>
2024-10-06 09:14:16 +0700

    dpa_function.cpp

---
+++ dpa_function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double luaslingkaran(double jari){
+//return (jari *jari * 3.14);
+return pow(jari,2)*3.14;
+}
+
+double luaspersegi(double sisi){
+return pow(sisi,2);
+}
+int main(){
+int jari;
+cout << "masukkan jari-jari" << endl;
+cin >> jari;
+
+cout << "hasilnya adalah : " << luaslingkaran(jari) << endl;
+
+
+return 0;
+}
